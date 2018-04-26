66c0a823939f38e73900c6a742d28c241191ffd7
Daksha Patel <dakshapatel100@gmail.com>
2018-04-26 22:39:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,7 +16,7 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board,index)
-  if index.between?(0,8) && !position_taken(board,index)
+  if index.between?(0,8) && !position_taken?(board,index)
     return true
   end
 end
@@ -28,4 +28,5 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+
 end
